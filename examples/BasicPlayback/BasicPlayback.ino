fa4e45e4555208920d8dc9b466fd48bcaef9f9d1
atmospheng <atmospheng@gmail.com>
2026-01-03 15:21:24 +0800

    Ported to Cheap Yellow Display ESP32 CYD to stream Serial MIDI 31250 8-n-1 to S2 on connector P3

--- examples/BasicPlayback/BasicPlayback.ino
+++ examples/BasicPlayback/BasicPlayback.ino
@@ -5,14 +5,79 @@
 
 // Serial commands: play, pause, resume, stop
 
+// DEFINE ****************************************************************************************
+
+#define BITS_PER_BYTE 8
+
+#define MIDI_MSG_BYTE_NOTE_OFF            0x80 // 
+#define MIDI_MSG_BYTE_NOTE_ON             0x90
+#define MIDI_MSG_STATUS_CONTROL_CHANGE    0xb0
+#define MIDI_MSG_STATUS_PROGRAM_CHANGE    0xc0
+#define MIDI_MSG_STATUS_PITCH_BEND        0xe0
+
+
+//#define TARGET_ESP32_KS0413
+#define TARGET_ESP32_CYD
+//#define TARGET_ESP32_T7_S3
+//#define TARGET_XIAO_SAMD21 // NOTE - NOT AN ESP32 - 2026-01-03 NOK - THIS FAILS AT LOADING A MIDI FILE. WHY?
+
+//#define FS_LITTLEFS 
+#define FS_SD 
+
+#define DEBUG_SERIAL_OUT
+#define SERIAL_MIDI_STREAM_OUT
+#define MIDI_CHANNEL_SPECIFIC // allow channel (0-15) specification in the msg, otherwise its all notes on channel 0.
+
+#ifdef TARGET_XIAO_SAMD21
+#define SERIAL_MIDI Serial1 //  for xiao samd21
+#else
+#define SERIAL_MIDI Serial2
+#endif
+
+#define MUTED_CHANNEL_A 9 // equivalent as '10' for 1-16. Drum channel.
+
+// INCLUDE ****************************************************************************************
+
+#ifdef FS_LITTLEFS 
 
 #include <LittleFS.h>
-#include "ESP32MidiPlayer.h"
-
 ESP32MidiPlayer midiPlayer(LittleFS); // Use LittleFS
 
-const char* MIDI_FILE = "/test.mid";
+#elif defined(FS_SD)
 
+#if defined(TARGET_XIAO_SAMD21) 
+#include <SPI.h> //  for Seeed Xiao samd21
+// #include <SD.h> 
+#include <Seeed_Arduino_FS.h> 
+#define SD_CS 2  // xiao samd21 expansion board
+
+#elif defined(TARGET_ESP32_CYD)
+#include <SD.h> 
+#include <FS.h> 
+#define SD_CS 5  // cyd sd_cs
+
+// #include "ESP32MidiPlayer.h"
+#include "src/ESP32MidiPlayer.h" //  mod
+
+ESP32MidiPlayer midiPlayer(SD); // Use SD and then FS
+
+#else // default
+#include <SD.h> 
+#include <FS.h> 
+#define SD_CS 5 
+
+#endif // if defined(TARGET_XIAO_SAMD21) 
+
+#endif // FS_LITTLEFS
+
+
+// const char* MIDI_FILE = "/small.mid";
+const char* MIDI_FILE = "/file001.mid"; //  copy of Pachelbel.mid
+// const char* MIDI_FILE = "/test.mid";
+// const char* MIDI_FILE = "/song.mid";
+
+
+// FUNCTIONS **************************************************************************************
 
 void handleLog(MidiLogLevel level, const char* message) {
   const char* levelStr = "";
@@ -41,62 +106,228 @@ void handleLog(MidiLogLevel level, const char* message) {
   Serial.printf("%s%s\n", levelStr, message);
 }
 
+
 void handleNoteOn(uint8_t channel, uint8_t note, uint8_t velocity) {
-Serial.printf("[EVT] Note On:  Ch=%u Note=%u Vel=%u (Tick: %lu)\n",
-channel + 1, note, velocity, midiPlayer.getCurrentTick()); // Display channel 1-16
-}
+
+  if(channel != MUTED_CHANNEL_A){ // specify channel to be muted.
+
+#ifdef DEBUG_SERIAL_OUT
+    Serial.printf("[EVT] Note On:  Ch=%u Note=%u Vel=%u (Tick: %lu)\n",
+    channel + 1, note, velocity, midiPlayer.getCurrentTick()); // Display channel 1-16
+#endif // DEBUG_SERIAL_OUT
+
+#ifdef SERIAL_MIDI_STREAM_OUT
+
+#ifdef MIDI_CHANNEL_SPECIFIC
+    SERIAL_MIDI.write((MIDI_MSG_BYTE_NOTE_ON & 0xf0) | ((channel + 1) & 0x0f)); // note on, merge channel number in to message.
+#else
+    SERIAL_MIDI.write(MIDI_MSG_BYTE_NOTE_ON); // note on, channel "0" = channel 1  = all channels
+#endif // MIDI_CHANNEL_SPECIFIC
+
+    SERIAL_MIDI.write(note); // note value
+    SERIAL_MIDI.write(velocity); // velocity value
+
+#endif // SERIAL_MIDI_STREAM_OUT 
+  } // if channel ...
+} // handleNoteOn
 
 void handleNoteOff(uint8_t channel, uint8_t note, uint8_t velocity) {
-Serial.printf("[EVT] Note Off: Ch=%u Note=%u Vel=%u (Tick: %lu)\n",
-channel + 1, note, velocity, midiPlayer.getCurrentTick()); // Display channel 1-16
-}
+
+  if(channel != MUTED_CHANNEL_A){ // specify channel to be muted
+
+#ifdef DEBUG_SERIAL_OUT
+    Serial.printf("[EVT] Note Off: Ch=%u Note=%u Vel=%u (Tick: %lu)\n",
+    channel + 1, note, velocity, midiPlayer.getCurrentTick()); // Display channel 1-16
+#endif // DEBUG_SERIAL_OUT
+
+#ifdef SERIAL_MIDI_STREAM_OUT
+
+    #ifdef MIDI_CHANNEL_SPECIFIC
+    SERIAL_MIDI.write((MIDI_MSG_BYTE_NOTE_OFF & 0xf0) | ((channel + 1) & 0x0f)); // note on, merge channel number in to message.
+#else
+    SERIAL_MIDI.write(MIDI_MSG_BYTE_NOTE_OFF); // note off, channel "0" = channel 1 = all channels
+    #endif // MIDI_CHANNEL_SPECIFIC
+
+    SERIAL_MIDI.write(note); // note value
+    SERIAL_MIDI.write(velocity); // velocity value
+
+#endif // SERIAL_MIDI_STREAM_OUT
+  } // if channel ...
+
+} // handleNoteOff
+
 
 void handleControlChange(uint8_t channel, uint8_t controller, uint8_t value) {
+
+#ifdef DEBUG_SERIAL_OUT
 Serial.printf("[EVT] Ctrl Chg: Ch=%u CC=%u Val=%u (Tick: %lu)\n",
 channel + 1, controller, value, midiPlayer.getCurrentTick()); // Display channel 1-16
-}
+#endif // DEBUG_SERIAL_OUT
+
+#ifdef SERIAL_MIDI_STREAM_OUT
+
+#ifdef MIDI_CHANNEL_SPECIFIC
+SERIAL_MIDI.write((MIDI_MSG_STATUS_CONTROL_CHANGE & 0xf0) | ((channel + 1) & 0x0f)); // note on, merge channel number in to message.
+#else
+SERIAL_MIDI.write(MIDI_MSG_STATUS_CONTROL_CHANGE);
+#endif // MIDI_CHANNEL_SPECIFIC
+
+SERIAL_MIDI.write(controller); // data byte 1
+SERIAL_MIDI.write(value); // data byte 2
+
+#endif // SERIAL_MIDI_STREAM_OUT
+
+} // handleControlChange
 
 void handleProgramChange(uint8_t channel, uint8_t program) {
+
+#ifdef DEBUG_SERIAL_OUT
 Serial.printf("[EVT] Prog Chg: Ch=%u Prog=%u (Tick: %lu)\n",
 channel + 1, program, midiPlayer.getCurrentTick()); // Display channel 1-16
-}
+#endif // DEBUG_SERIAL_OUT
+
+#ifdef SERIAL_MIDI_STREAM_OUT
+
+#ifdef MIDI_CHANNEL_SPECIFIC
+SERIAL_MIDI.write((MIDI_MSG_STATUS_PROGRAM_CHANGE & 0xf0) | ((channel + 1) & 0x0f)); // note on, merge channel number in to message.
+#else
+SERIAL_MIDI.write(MIDI_MSG_STATUS_PROGRAM_CHANGE);
+#endif // MIDI_CHANNEL_SPECIFIC
+
+SERIAL_MIDI.write(program); // data byte 1
+
+#endif // SERIAL_MIDI_STREAM_OUT
+
+} // handleProgramChange
 
 void handlePitchBend(uint8_t channel, int16_t value) {
+
+#ifdef DEBUG_SERIAL_OUT
 Serial.printf("[EVT] Pitch Bnd: Ch=%u Val=%d (Tick: %lu)\n",
 channel + 1, value, midiPlayer.getCurrentTick()); // Display channel 1-16
-}
+#endif // DEBUG_SERIAL_OUT
+
+#ifdef SERIAL_MIDI_STREAM_OUT
+
+#ifdef MIDI_CHANNEL_SPECIFIC
+SERIAL_MIDI.write((MIDI_MSG_STATUS_PITCH_BEND & 0xf0) | ((channel + 1) & 0x0f)); // note on, merge channel number in to message.
+#else
+SERIAL_MIDI.write(MIDI_MSG_STATUS_PITCH_BEND);
+#endif // MIDI_CHANNEL_SPECIFIC
+
+SERIAL_MIDI.write(value & 0x00ff); // data byte 1 - ls byte
+SERIAL_MIDI.write((value & 0xff00 >> BITS_PER_BYTE)); // data byte 1 - ms byte
+
+#endif // SERIAL_MIDI_STREAM_OUT
+
+} // handlePitchBend
 
 void handleTempoChange(uint32_t microsecondsPerQuarterNote) {
+
+#ifdef DEBUG_SERIAL_OUT
 float bpm = 60000000.0f / microsecondsPerQuarterNote;
 Serial.printf("[EVT] Tempo Chg: %lu us/qn (%.2f BPM) (Tick: %lu)\n",
 microsecondsPerQuarterNote, bpm, midiPlayer.getCurrentTick());
-}
+#endif // DEBUG_SERIAL_OUT
+
+} // handleTempoChange
 
 void handleTimeSignature(uint8_t num, uint8_t den_pow2, uint8_t clocks, uint8_t b) {
+
+#ifdef DEBUG_SERIAL_OUT
 Serial.printf("[EVT] Time Sig: %u/%u Clocks=%u 32nds/QN=%u (Tick: %lu)\n",
 num, (1 << den_pow2), clocks, b, midiPlayer.getCurrentTick());
-}
+#endif // DEBUG_SERIAL_OUT
+
+} // handleTimeSignature
 
 void handleEndOfTrack(uint8_t trackIndex) {
+
+#ifdef DEBUG_SERIAL_OUT
 Serial.printf("[INF] EndOfTrack reached for track %u (Tick: %lu)\n",
 trackIndex, midiPlayer.getCurrentTick());
-}
+#endif // DEBUG_SERIAL_OUT
+
+} // handleEndOfTrack
 
 void handlePlaybackComplete() {
+
+#ifdef DEBUG_SERIAL_OUT
 Serial.println("\n[INF] === Playback Finished ===\n");
-}
+#endif // DEBUG_SERIAL_OUT
+
+} // handlePlaybackComplete
+
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
+  Serial.printf("Listing directory: %s\n", dirname);
+
+  File root = fs.open(dirname);
+  if (!root) {
+    Serial.println("Failed to open directory");
+    return;
+  }
+  if (!root.isDirectory()) {
+    Serial.println("Not a directory");
+    return;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+      if (levels) { // Recursively list subdirectories if levels > 0
+        listDir(fs, file.name(), levels - 1);
+      }
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("  SIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+} // listDir  added
+
 
 void setup() {
   Serial.begin(115200);
-  while (!Serial) delay(10);
-  delay(1000);
+
+  //  while (!Serial) delay(10);
+
+//  Serial2.begin(115200, SERIAL_8N1, RX, TX);
+#ifdef TARGET_ESP32_KS0413 
+  Serial2.begin(115200, SERIAL_8N1, 16, 17); // (BAUD) (BITS) (RXD2 - GPIO-16) (TXD2 - GPIO-17)
+
+#elif defined (TARGET_ESP32_CYD) 
+  // used by USB COM Serial2.begin(115200, SERIAL_8N1, 3, 1); // (BAUD) (BITS) (RXD2 - U0RXD - GPIO-3) (TXD2 - U0TXD - GPIO-1)
+  // Serial2.begin(115200, SERIAL_8N1, 35, 22); //  CYD connector P3 (BAUD) (BITS) (RXD - GPIO-35) (TXD - GPIO-22)
+  Serial2.begin(31250, SERIAL_8N1, 35, 22); //  CYD connector P3 (BAUD) (BITS) (RXD - GPIO-35) (TXD - GPIO-22)
+
+#elif defined (TARGET_T7_S3)
+  Serial2.begin(115200, SERIAL_8N1, 47, 48); //  T7_S3 connector D (BAUD) (BITS) (RXD - GPIO-47) (TXD - GPIO-48)
+
+#elif defined (TARGET_XIAO_SAMD21)
+  SERIAL_MIDI.begin(31250);
+//  SERIAL_MIDI.begin(115200);
+//  SERIAL_MIDI.begin(115200, SERIAL_8N1, 7, 6); //  (BAUD) (BITS) (RXD - GPIO-7) (TXD - GPIO-6)
+#endif
+
+
+//  delay(1000);
+  delay(2000); //  allow extra time for Windows to register the COM port
 
   Serial.println("Initializing MIDI Player...");
 
+  Serial.flush(); 
+  delay(1000); 
+
+  pinMode(SD_CS, OUTPUT); 
 
   // Enable logging callback (set to info by default)
-  //midiPlayer.setLogCallback(handleLog);
-  //midiPlayer.setLogLevel(MidiLogLevel::INFO);
+  // midiPlayer.setLogCallback(handleLog); //  comment added
+  // midiPlayer.setLogLevel(MidiLogLevel::INFO); //  comment added
 
   midiPlayer.setNoteOnCallback(handleNoteOn);
   midiPlayer.setNoteOffCallback(handleNoteOff);
@@ -108,18 +339,41 @@ void setup() {
   midiPlayer.setEndOfTrackCallback(handleEndOfTrack);
   midiPlayer.setPlaybackCompleteCallback(handlePlaybackComplete);
 
+#ifdef FS_LITTLEFS // 
   if (!LittleFS.exists(MIDI_FILE)) {
     Serial.printf("MIDI file %s not found in LittleFS!\n", MIDI_FILE);
-    while (true) delay(1000);
+    //  while (true) delay(1000);
   }
+
+#elif defined(FS_SD) 
+  // Initialize SD card
+
+  if (!SD.begin(SD_CS)) { // SD_CS is the Chip Select pin for your SD card module
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+  else{
+   // Call the function to list the root directory with a certain level of recursion
+  listDir(SD, "/", 0); // Lists root directory, 0 for no recursion into subdirectories. Lists to Serial. 
+  }
+
+#endif //  FS
 
   if (!midiPlayer.load(MIDI_FILE)) {
     Serial.printf("Failed to load MIDI file %s\n", MIDI_FILE);
     while (true) delay(1000);
   }
+  else{
+      Serial.printf("Successful load MIDI file %s\n", MIDI_FILE);
+  }
 
   Serial.println("MIDI Player ready. Commands: play, pause, resume, stop");
-}
+
+  // Serial2.println("play"); //  automatically play the current file ?
+  midiPlayer.play();
+  Serial.println("Playback started");
+
+} // setup  comment added
 
 void handleSerialCommands() {
   if (Serial.available() > 0) {
@@ -162,3 +416,5 @@ void loop() {
   midiPlayer.tick(); // Process MIDI events
   handleSerialCommands(); // Handle serial input
 }
+
+// END_OF_FILE
---
+++ examples/BasicPlayback/src/ESP32MidiPlayer.h
@@ -0,0 +1,151 @@
+#ifndef ESP32MidiPlayer_H
+#define ESP32MidiPlayer_H
+
+#include <Arduino.h>
+
+#include <FS.h>
+
+#include <vector>
+#include <cstdarg> // For va_list
+
+// --- Log Level Definition ---
+enum class MidiLogLevel {
+    NONE = -1,  // Disable all logging
+    ERROR = 0, // Critical errors that prevent operation
+    WARN = 1,  // Warnings about potential issues or unexpected data
+    INFO = 2,  // General informational messages (file loaded, playback start/stop)
+    DEBUG = 3, // Detailed step-by-step debugging information
+    VERBOSE = 4// Even more detailed info (e.g., raw byte reads - not used much here yet)
+};
+
+// --- Callback Function Pointer Types ---
+// LogCallback now accepts the level
+typedef void (*LogCallback)(MidiLogLevel level, const char* message);
+typedef void (*NoteOnCallback)(uint8_t channel, uint8_t note, uint8_t velocity);
+typedef void (*NoteOffCallback)(uint8_t channel, uint8_t note, uint8_t velocity);
+typedef void (*ControlChangeCallback)(uint8_t channel, uint8_t controller, uint8_t value);
+typedef void (*ProgramChangeCallback)(uint8_t channel, uint8_t program);
+typedef void (*PitchBendCallback)(uint8_t channel, int16_t value); // +/- 8192
+typedef void (*TempoChangeCallback)(uint32_t microsecondsPerQuarterNote);
+typedef void (*TimeSignatureCallback)(uint8_t numerator, uint8_t denominator_pow2, uint8_t clocksPerMetronome, uint8_t thirtySecondNotesPerQuarter); // Keep denominator_pow2 for raw MIDI value if preferred
+typedef void (*EndOfTrackCallback)(uint8_t trackIndex); // Called when a track finishes
+typedef void (*PlaybackCompleteCallback)(); // Called when all tracks finish
+
+
+// --- Playback State Enum ---
+enum class PlaybackState {
+    STOPPED,
+    PLAYING,
+    PAUSED
+};
+
+// --- Track Info Structure ---
+struct TrackInfo {
+    uint32_t startOffset = 0;
+    uint32_t currentOffset = 0;
+    uint64_t nextEventTick = 0; // Use 64-bit for potentially very long files/high tick counts
+    uint8_t lastStatusByte = 0;
+    bool endOfTrackReached = false;
+};
+
+class ESP32MidiPlayer {
+public:
+    // Constructor - Takes the filesystem to use (e.g., LittleFS)
+    ESP32MidiPlayer(FS& filesystem);
+    ~ESP32MidiPlayer();
+
+    // --- Configuration ---
+    void setLogCallback(LogCallback callback);
+    void setLogLevel(MidiLogLevel level);      // Method to set log level
+    void setNoteOnCallback(NoteOnCallback callback);
+    void setNoteOffCallback(NoteOffCallback callback);
+    void setControlChangeCallback(ControlChangeCallback callback);
+    void setProgramChangeCallback(ProgramChangeCallback callback);
+    void setPitchBendCallback(::PitchBendCallback callback);
+    void setTempoChangeCallback(TempoChangeCallback callback);
+    void setTimeSignatureCallback(TimeSignatureCallback callback);
+    void setEndOfTrackCallback(EndOfTrackCallback callback);
+    void setPlaybackCompleteCallback(PlaybackCompleteCallback callback);
+
+    // --- File Handling & Playback Control ---
+    bool load(const char* filename); // Load MIDI file header and prepare tracks
+    void play();                     // Start playback from the beginning or resume if paused
+    void pause();                    // Pause playback
+    void resume();                   // Resume playback (alias for play() when paused)
+    void stop();                     // Stop playback and reset
+
+    // --- Main Loop Update ---
+    // This MUST be called frequently in the main loop()
+    void tick();
+
+    // --- Status Queries ---
+    PlaybackState getState() const;
+    bool isPlaying() const;
+    bool isPaused() const;
+    uint32_t getCurrentTick() const; // Get the current playback position in MIDI ticks
+    uint32_t getTempo() const; // Get current tempo in Microseconds Per Quarter Note
+
+private:
+    // --- Private Helper Methods ---
+    void _resetPlaybackState();
+    bool _parseFileHeader();
+    bool _prepareTracks();
+    uint32_t _readVariableLengthQuantity(uint32_t& offset); // Reads from currentOffset of a track
+    uint32_t _readBytes(uint32_t offset, uint8_t* buffer, uint32_t length); // Reads from file
+    uint32_t _peekBytes(uint32_t offset, uint8_t* buffer, uint32_t length); // Peeks from file
+    uint8_t _readUint8(uint32_t& offset);
+    uint16_t _readUint16BE(uint32_t& offset); // Read Big Endian Short
+    uint32_t _readUint32BE(uint32_t& offset); // Read Big Endian Long
+    void _processNextEvent();
+    int _findTrackWithNextEvent() const; // Returns index of track with earliest nextEventTick, or -1
+    // Updated signature:
+    void _handleMidiEvent(uint8_t trackIndex, uint8_t statusByte, uint32_t& trackOffset, bool runningStatusUsed, uint8_t data1_val);
+    void _handleMetaEvent(uint8_t trackIndex, uint32_t& trackOffset);
+    void _handleSysexEvent(uint8_t trackIndex, uint8_t type, uint32_t& trackOffset);
+    void _advanceTickTime();
+    // Updated signature:
+    void _log(MidiLogLevel level, const char* format, ...); // Internal logging helper
+
+    // --- Member Variables ---
+    FS& _fs;                     // Filesystem reference
+    File _midiFile;              // Current MIDI file handle
+    String _filename = "";       // Current filename
+    PlaybackState _state = PlaybackState::STOPPED;
+    MidiLogLevel _currentLogLevel = MidiLogLevel::INFO; // Default log level
+
+    // MIDI Header Info
+    uint16_t _format = 0;
+    uint16_t _trackCount = 0;
+    uint16_t _division = 96;    // Ticks Per Quarter Note (TPQN)
+
+    // Playback Timing
+    uint32_t _microsecondsPerQuarterNote = 500000; // Default: 120 BPM
+    uint64_t _currentTick = 0;
+    uint64_t _playbackStartMicros = 0;
+    uint64_t _lastEventMicros = 0;
+    uint64_t _pauseStartMicros = 0; // To calculate paused duration
+
+    // Track Data
+    std::vector<TrackInfo> _tracks;
+    uint8_t _finishedTracks = 0; // Count of tracks that reached EOT
+
+    // Callbacks
+    LogCallback _logCallback = nullptr;
+    NoteOnCallback _noteOnCallback = nullptr;
+    NoteOffCallback _noteOffCallback = nullptr;
+    ControlChangeCallback _controlChangeCallback = nullptr;
+    ProgramChangeCallback _programChangeCallback = nullptr;
+	::PitchBendCallback _pitchBendCallback = nullptr;
+    TempoChangeCallback _tempoChangeCallback = nullptr;
+    TimeSignatureCallback _timeSignatureCallback = nullptr;
+    EndOfTrackCallback _endOfTrackCallback = nullptr;
+    PlaybackCompleteCallback _playbackCompleteCallback = nullptr;
+
+    // Internal buffer (optional)
+    // uint8_t _readBuffer[64];
+    // uint32_t _bufferOffset = 0;
+    // uint32_t _bufferSize = 0;
+    // uint32_t _bufferFilePos = 0;
+};
+
+#endif // ESP32_MIDI_PLAYER_H
